586381960864de0ab97547ae3ad929a8e023a924
Gobinath <princegopinath42@gmail.com>
2023-09-11 23:54:35 +0530

    feat: list component

--- react-app/src/components/List.tsx
+++ react-app/src/components/List.tsx
@@ -1,15 +1,22 @@
-function List(){
-    const bikes = ["duke390","cb unicorn","vikrant"];
-    
-     return(
-        
-       <> 
-       <h1>Bikes</h1>
-        <ul className="list-group">
-          {bikes.map(bike=><li key={bike} onClick={()=>{console.log(bike + "clicked")}} className="list-group-item">{bike}</li>)}      
+import { MouseEventHandler } from "react";
+function List() {
+  const bikes = ["duke390", "cb unicorn", "vikrant"];
+  const handleEvent = (event: React.MouseEvent<HTMLLIElement>) => {
+    console.log(event);
+  };
+
+  return (
+    <>
+      <h1>Bikes</h1>
+      <ul className="list-group">
+        {bikes.map((bike) => (
+          <li key={bike} onClick={handleEvent} className="list-group-item">
+            {bike}
+          </li>
+        ))}
       </ul>
-      </>
-     );
-};
+    </>
+  );
+}
 
 export default List;
