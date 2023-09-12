6ba932998d45cac9056dc8b2fda765f1bd884ea3
Gobinath <princegopinath42@gmail.com>
2023-09-12 19:01:15 +0530

    state and props

--- react-app/src/components/List.tsx
+++ react-app/src/components/List.tsx
@@ -1,16 +1,22 @@
-import { MouseEventHandler } from "react";
-function List() {
-  const bikes = ["duke390", "cb unicorn", "vikrant"];
+import { MouseEventHandler, useState } from "react";
+
+interface Props{
+  bikes : string[],
+  heading : string
+} 
+function List({bikes,heading}:Props) {
+  
   const handleEvent = (event: React.MouseEvent<HTMLLIElement>) => {
     console.log(event);
   };
 
+const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
       <h1>Bikes</h1>
       <ul className="list-group">
-        {bikes.map((bike) => (
-          <li key={bike} onClick={handleEvent} className="list-group-item">
+        {bikes.map((bike,index) => (
+          <li key={bike} onClick={()=>{setSelectedIndex(index);}} className={selectedIndex == index? "list-group-item active" : "list-group-item"}>
             {bike}
           </li>
         ))}
